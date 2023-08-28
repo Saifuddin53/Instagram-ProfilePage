8cb34a8b62eaa5578fd2da43527eac67e260e80a
Saifuddin <adenwala32@gmail.com>
2023-08-28 10:03:52 +0530

    profile screen updated for preview

--- app/src/main/java/com/example/instagramui/ProfileScreen.kt
+++ app/src/main/java/com/example/instagramui/ProfileScreen.kt
@@ -1,8 +1,15 @@
 package com.example.instagramui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ProfileScreen() {
 
+}
+
+@Preview
+@Composable
+fun ProfileScreenPreview1(){
+    ProfileScreen()
 }
